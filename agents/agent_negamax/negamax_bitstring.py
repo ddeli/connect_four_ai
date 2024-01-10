54c120e36bef31eda33aeb8f95252f699b73268c
ddeli <d.deli@campus.tu-berlin.de>
2024-01-10 21:07:03 +0100

    convert the negamax_algorithm to bitstring

--- agents/agent_negamax/negamax_bitstring.py
+++ agents/agent_negamax/negamax_bitstring.py
@@ -7,15 +7,13 @@ from agents.agent_minimax_sahand.heuristic import evaluate_board
 from typing import Optional, Callable
 from bitstring import board_to_bitstring, apply_player_action_bitstring, \
     calculate_score_bitstring, bitstring_to_board, check_end_state_bitstring, \
-    copy_bitstring_array
+    copy_bitstring_array, get_valid_moves_bitstring
 
 PlayerView = np.int8
 MaxView = PlayerView(1)
 MinView = PlayerView(-1)
 
 MaxDepth = 4
-
-
 
 
 class Node():
@@ -78,14 +76,13 @@ def iterative_deepening_bitstring(board, agent_piece: BoardPiece, maxdepth:int =
     maxdepth = MaxDepth
     set_players_pieces(agent_piece)
     parent_board = board_to_bitstring(board)
-    #parent_board = copy.deepcopy(board)
+    # parent_board = copy.deepcopy(board)
     mindepth = maxdepth if Node.skip_iterative_deepening else 1
     for depth in range(mindepth, maxdepth+1):
         Node.reset()
         print(f'\n***start analysing depth: {depth} ***')
         Node(Node.nodenumber, parent_board, depth)
-        fornowbacktoboard = bitstring_to_board(board,agent_piece)
-        negamax(fornowbacktoboard, depth)
+        negamax(parent_board, depth)
         Node.pv = []
         get_pv()
         if depth == maxdepth:
@@ -98,56 +95,65 @@ def set_players_pieces(agent_piece):
     Node.agent_piece = agent_piece
     Node.opponent_piece = PLAYER2 if agent_piece == PLAYER1 else PLAYER1
 
+
 def check_terminal(board, playerview):
     # note that playerview is one move ahead of the last player.
     # minimizer checks the result for maximizer's last move and vice versa.
     lastpiece = Node.agent_piece if playerview == MinView else Node.opponent_piece
     terminal = False
     terminal_score = None
-    lastmove_result = check_end_state(board,lastpiece)
+    lastmove_result = check_end_state_bitstring(board, lastpiece)
     # from the minimizer's point of view, maximizer's win is alway -1000.
     # the same is true for maximizer's point of view.
-    if lastmove_result == GameState.IS_WIN: terminal, terminal_score = True, -1000
-    elif lastmove_result == GameState.IS_DRAW: terminal, terminal_score = True, 0
+    if lastmove_result == GameState.IS_WIN:
+        terminal, terminal_score = True, -1000
+    elif lastmove_result == GameState.IS_DRAW:
+        terminal, terminal_score = True, 0
     return terminal, terminal_score
+
 
 def get_player_piece(playerview):
     player_piece = Node.agent_piece if playerview == 1 else Node.opponent_piece
     return player_piece
 
 
-def negamax(parent_board, depth:int, alpha:float = float('-inf'), beta:float = float('inf'), playerview:PlayerView = MaxView):
+def negamax(parent_board, depth: int, alpha: float = float('-inf'), beta: float = float('inf'), playerview: PlayerView = MaxView):
     Node.hitcount += 1
 
     terminal, terminal_score = check_terminal(parent_board, playerview)
-    if terminal: return -terminal_score, Node.nodenumber
+    if terminal:
+        return -terminal_score, Node.nodenumber
     elif depth == 0:  
-        leaf_score = evaluate_board(parent_board,Node.agent_piece)*playerview
+        # TEMP----------------------UNTIL HEURISTIC IS CONVERTED TO BITSTRING
+        # leaf_score = evaluate_board(parent_board, Node.agent_piece)*playerview
+        leaf_score = calculate_score_bitstring(parent_board)*playerview
+        # TEMP----------------------UNTIL HEURISTIC IS CONVERTED TO BITSTRING
         Node.instances[Node.nodenumber].leaf_score = leaf_score
         return -leaf_score, Node.nodenumber
     
     best_score = float('-inf')
 
     best_move = None
-    moves = get_valid_moves(parent_board)
+    moves = get_valid_moves_bitstring(parent_board)
     moves = order_moves(moves)
     current_nodenumber = Node.nodenumber
     first_move = True
     for move in moves:
-        child_board = copy.deepcopy(parent_board)
+        child_board = copy_bitstring_array(parent_board)
         player_piece = get_player_piece(playerview)
-        apply_player_action(child_board, move, player_piece)
+        apply_player_action_bitstring(child_board, move, player_piece)
         Node.nodenumber += 1
-        Node(Node.nodenumber, child_board, depth-1, parent=current_nodenumber, parent_move=move)
+        #TODO Check if playerpiece in bitstring_to_board function below is right
+        Node(Node.nodenumber, bitstring_to_board(child_board, player_piece), depth-1, parent=current_nodenumber, parent_move=move)
 
         if first_move or Node.skip_null_window:
-            board_score, child_nodenumber = negamax(child_board,depth-1, -beta, -alpha, -playerview)
+            board_score, child_nodenumber = negamax(child_board, depth-1, -beta, -alpha, -playerview)
         else:
             mark_nodenumber = Node.nodenumber
-            board_score, child_nodenumber = negamax(child_board,depth-1, -(alpha+1), -alpha, -playerview)
+            board_score, child_nodenumber = negamax(child_board, depth-1, -(alpha+1), -alpha, -playerview)
             if alpha < board_score < beta:
                 Node.nodenumber = mark_nodenumber
-                board_score, child_nodenumber = negamax(child_board,depth-1, -beta, -alpha, -playerview)
+                board_score, child_nodenumber = negamax(child_board, depth-1, -beta, -alpha, -playerview)
 
         best_score, best_move = update_bestscore_bestmove(board_score, best_score, move, best_move, current_nodenumber, child_nodenumber)
         prune, alpha, beta = check_prune(board_score, alpha, beta)
@@ -229,11 +235,4 @@ def apply_move(child_board, move, playerview:PlayerView):
     else: child_board[1].append(move)
     return child_board
 
-def get_valid_moves(board: np.ndarray) -> list[int]:
-    # return [0,1]
-    is_open = board[-1, :] == 0
-    possible_moves = np.arange(BOARD_COLS)
-    valid_moves = possible_moves[is_open]
-    valid_moves = list(valid_moves)
-    return valid_moves
 
