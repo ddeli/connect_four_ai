1165a07982e75e14f469d0cded68815aac384519
Sahand <sahand.sabahi@gmail.com>
2024-02-09 18:51:54 +0100

    added timing to the test_all_in_place()

--- tests/test_negamax/test_negamax_bitboard.py
+++ tests/test_negamax/test_negamax_bitboard.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import time
 from game_utils import PLAYER1, PLAYER2, PLAYER1_PRINT, PLAYER2_PRINT
 from bitstring import board_to_bitstring
 from agents.agent_negamax.negamax_bitstring_heuristics import Node
@@ -160,7 +161,8 @@ def test_order_moves_pv():
 
 def test_all_in_place():
     """
-    this is used for step by step sanity checks and debugging
+    this is used for step by step sanity checks and debugging.
+    also for timing the agent in different scenarios and for different inputs.
     """
     board_string = ''' 
      - - - - - - - 
@@ -168,8 +170,8 @@ def test_all_in_place():
     |             |
     |             |
     |             |
-    |             |
-    |  X X   X    |
+    |      O      |
+    |      X X    |
      - - - - - - -
      0 1 2 3 4 5 6
     '''
@@ -179,7 +181,21 @@ def test_all_in_place():
     print(bit_board)
 
     Node.skip_order = False
-    Node.skip_null_window = False
-    Node.skip_iterative_deepening = False
+    Node.skip_null_window = True
+    Node.skip_iterative_deepening = True
 
-    iterative_deepening_bitstring(board, agent_piece=PLAYER1)
+    max_depth = 7
+    agent_piece = PLAYER2
+    start_time = time.time()
+    best_move, Node.instances = iterative_deepening_bitstring(board, agent_piece,maxdepth=max_depth)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(board_string)
+    print(f'player: {agent_piece}')
+    print(f'iterative deepening: {Node.skip_iterative_deepening}')
+    print(f'Null Window: {Node.skip_null_window}')
+    print(f'elapsed time is {elapsed_time}')
+    print(f'Nodenumber is {Node.nodenumber}')
+    print(f'Nodes visited are {Node.hitcount}')
+    print(f'max depth is {max_depth}')
+    print(f'principle variation: {Node.pv}')
