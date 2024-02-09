dcc0d4c6e274f1bee2c5eb87d30deaed59625da5
Sahand <sahand.sabahi@gmail.com>
2024-02-09 12:08:03 +0100

    test order_moves()

--- tests/test_negamax/test_negamax_bitboard.py
+++ tests/test_negamax/test_negamax_bitboard.py
@@ -1,8 +1,73 @@
 import numpy as np
-from agents.agent_negamax.negamax_bitstring import Node, iterative_deepening_bitstring
-from game_utils_sahand import PLAYER1, PLAYER2, string_to_board
-from bitstring import board_to_bitstring, bitstring_to_board
+import random
+from game_utils import PLAYER1, PLAYER2, PLAYER1_PRINT, PLAYER2_PRINT
+from bitstring import board_to_bitstring
+from agents.agent_negamax.negamax_bitstring_heuristics import Node
+from agents.agent_negamax.negamax_bitstring_heuristics import iterative_deepening_bitstring, order_moves
 
+def string_to_board(pp_board: str):
+    """
+    Takes the output of pretty_print_board and turns it back into an ndarray.
+    This is quite useful for debugging, when the agent crashed and you have the last
+    board state as a string.
+
+    Parameters
+    ----------
+    pp_board : str
+        The string representation of a game board as produced by pretty_print_board.
+
+    Returns
+    -------
+    board_array: numpy.ndarray
+        A NumPy array representing the game board.
+    """
+
+    board_array_of_string = pp_board.split('|')[1::2]
+    board_array_of_string =np.array([[i for i in row] for row in board_array_of_string])[:,::2]
+
+    board_array = np.zeros(board_array_of_string.shape)
+    board_array[board_array_of_string==PLAYER1_PRINT] = PLAYER1
+    board_array[board_array_of_string==PLAYER2_PRINT] = PLAYER2
+    board_array = board_array[::-1]
+    return board_array
+
+
+def test_order_moves_pv():
+    Node.pv[:] = [1,3,4,5]
+    Node.principle_move_taken = 0
+
+    moves = [0,1,2,3,5]
+    print('\navaialbel moves:', moves)
+    moves_1 = order_moves(moves)
+    print(f'ordered move:',moves_1)
+    
+
+    moves = [0,1,2,3,5]
+    print('\navaialbel moves:', moves)
+    moves_2 = order_moves(moves)
+    print(f'ordered move:',moves_2)
+
+    moves = [0,1,2,3,5]
+    print('\navaialbel moves:', moves)
+    moves_3 = order_moves(moves)
+    print(f'ordered move:',moves_3)
+
+    moves = [0,1,2,3,5]
+    print('\navaialbel moves:', moves)
+    moves_4 = order_moves(moves)
+    print(f'ordered move:',moves_4)
+
+    moves = [0,1,2,3,5]
+    print('\navaialbel moves:', moves)
+    random.seed(0)
+    moves_5 = order_moves(moves,'random')
+    print(f'ordered move:',moves_5)
+    
+    assert moves_1 == [1, 0, 2, 3, 5]
+    assert moves_2 == [3, 0, 1, 2, 5]
+    assert moves_3 == [3, 2, 1, 5, 0]
+    assert moves_4 == [3, 2, 1, 5, 0]
+    assert moves_5 == [2, 1, 0, 5, 3]
 
 def test_all_in_place():
     board_string = ''' 
